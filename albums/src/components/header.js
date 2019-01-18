3db33eec17af8ced454f7b84402d8e3481bbee1f
Johnny Villegas <johnvillegas40@gmail.com>
2019-01-18 14:55:00 -0500

    Additional styles to the header

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,16 +1,24 @@
 // Import Librarys for making a component
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 // Make a component
 const Header = () => {
-    const {textStyle} = styles;
+    const {textStyle, viewStyle} = styles;
 
 
-    return <Text style={textStyle}>Albums!</Text>
+    return (
+        <View styles={viewStyle}>
+            <Text style={textStyle}>Albums!</Text>
+        </View>
+        
+    )
 }
 
 const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
     textStyle: {
         fontSize: 20
     }
