ae72695d881a40b8709d952726ec179957af4c61
John Villegas <jvillegas@foxwyn.com>
2019-01-21 15:51:46 -0500

    Added Axios, pulled data from api.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View,Text} from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List!!!</Text>
-        </View>
-    )
+class AlbumList extends Component {
+    componentWillMount(){
+        axios.get('https://rallyrallycoding.herokuapp.com/api/music_albums')
+        .then(res => console.log(res))
+    }
+    render(){
+        return (
+            <View>
+                <Text>Album List!!!</Text>
+            </View>
+        )
+    }
 };
 
 export default AlbumList;
