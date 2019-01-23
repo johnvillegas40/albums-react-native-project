27f6670b510f6306f5781cda5dfe0b1ef8a9e31b
Johnny Villegas <johnvillegas40@gmail.com>
2019-01-22 21:27:34 -0500

    Added function to Button, and refactored to be reusable.

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({album}) => {
-    const {title, artist, thumbnail_image, image} = album;
+    const {title, artist, thumbnail_image, image, url} = album;
     const {
         thumbnailStyle, 
         headerContentStyle, 
@@ -31,7 +31,9 @@ const AlbumDetail = ({album}) => {
             </CardSection>
 
             <CardSection>
-                <Button />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     )
@@ -56,7 +58,7 @@ const styles = {
         marginRight: 10
     },
     imageStyle: {
-        height: 300,
+        height: 400,
         flex: 1,
         width: null
     }
--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableOpacity} from 'react-native';
 
 
-const Button = () =>{
+const Button = ({onPress, children}) =>{
+    const {buttonStyle, textStyle} = styles;
+
     return(
-        <Text>Click Me!</Text>
+        <TouchableOpacity onPress={onPress} style={buttonStyle}>
+            <Text style={textStyle}>{children}</Text>
+        </TouchableOpacity>
     )
+}
+
+const styles = {
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#a4c639',
+        marginLeft: 5,
+        marginRight: 5,
+        elevation: 10
+    },
+    textStyle: {
+        alignSelf: 'center',
+        color: '#a4c639',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    }
 }
 export default Button;
